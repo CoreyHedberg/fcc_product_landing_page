62a2920fa9bec1e02a22a156d7c2731b3e578756
Corey Hedberg <chedberg@gmail.com>
2020-02-19 20:30:10 -0500

    Removed the Javascript

--- landing_page.js
+++
@@ -1,9 +0,0 @@
-/*
-    This website was written by Corey Hedberg for the purposes of the Free Code Camp Responsive Web Design Certification "Build a Product Landing Page" challenge.
-*/
-
-function getNewsletter() {
-  alert(
-    "Thank you for signing up for our newsletter. You will not be disappoointed!"
-  );
-}
